364f72a23d6368bb168c498aa40cf10fa67c3e3a
Tecookie <quanglikecookie@gmail.com>
2025-09-19 14:22:26 +0700

    commit

--- DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b5f546fe956e5a5e6a9b11ac37754149e54a5a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ea20eb78071e954cb91e0f99c4b6e6f41132c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
